8c612ecbc46513c5be88b26bb1bb48b630c86b8b
debbie-74 <96192372+debbie-74@users.noreply.github.com>
2023-01-17 13:00:25 +0400

    Updates

--- script2.js
+++ script2.js
@@ -77,35 +77,33 @@ ellipse(mouseX, mouseY, 50, 50);
 document.getElementById('space2').addEventListener('onclick', flingSp);
 document.getElementById('story').addEventListener('onclick', flingSt);
 document.getElementById('education').addEventListener('onclick', flingEd);
-document.getElementById('equity').addEventListener('onclick', flingEq);
+// document.getElementById('equity').addEventListener('onclick', flingEq);
 
 function flingSp() {
 document.getElementById('space2').style.animation = '1s away ease-in-out 0s 1 forwards';
 document.getElementById('story').style.display = 'block';
-txt.changeWord(["story"]);
-document.getElementById('spacetxt').innerHTML = "I thoroughly enjoy the act of storytelling, and it is at the core of most of the work I do, whether that be writing, filmmaking and digital media";
+txt.changeWord(["became a finalist"]);
+document.getElementById('spacetxt').innerHTML = "In 2020, I participated in the YouthTech startup incubator organised by StartAD, and my team was 1 of 15 teams chosen out of 98 applicants. In 2021, I was 1 of 6 people nominated to participate in StartAD's Innovention program";
 }
 
 function flingSt() {
 document.getElementById('story').style.animation = '1s away ease-in-out 0s 1 forwards';
 document.getElementById('education').style.display = 'block';
-txt.changeWord(["school"]);
-document.getElementById('spacetxt').innerHTML = "Learning new things is a significant part of my life and I am privileged to have access to good education. I am always looking for new opportunities to learn";
+txt.changeWord(["received a scholarship"]);
+document.getElementById('spacetxt').innerHTML = "In 2019, I was 1 of 150 students chosen out of 2000 applicants to participate in the Sutton Trust US programme. After participating, I successfully applied to New York University Abu Dhabi and was awarded a full scholarship. I graduate from NYUAD in 2023 with a major in Interactive Media and two minors in Creative Writing and Film and New Media";
 }
 
 function flingEd() {
 document.getElementById('education').style.animation = '1s away ease-in-out 0s 1 forwards';
-document.getElementById('equity').style.display = 'block';
-txt.changeWord(["equity"]);
-document.getElementById('spacetxt').innerHTML = "I believe in the idea that human beings should help each other in whatever way that we can. We are all privileged in some way, whether through our race, class, gender, sexuality and disability status. I am determined to help those who do not have the same privileges that I do";
-}
+document.getElementById('story').style.display = 'block';
+txt.changeWord(["story"]);
+// document.getElementById('spacetxt').innerHTML = "I believe in the idea that human beings should help each other in whatever way that we can. We are all privileged in some way, whether through our race, class, gender, sexuality and disability status. I am determined to help those who do not have the same privileges that I do";
 
-function flingEq() {
-document.getElementById('equity').style.animation = '1s away ease-in-out 0s 1 forwards';
 document.getElementById('space2').style.top = '27%';
 document.getElementById('space2').style.left = '10%';
 document.getElementById('space2').style.width = '30%';
 document.getElementById('space2').style.animation = '1s swing ease-in-out 0s infinite alternate';
+document.getElementById('education').style.display = 'none';
 
 document.getElementById('education').style.top = '27%';
 document.getElementById('education').style.left = '10%';
@@ -113,20 +111,15 @@ document.getElementById('education').style.width = '30%';
 document.getElementById('education').style.animation = '1s swing ease-in-out 0s infinite alternate';
 document.getElementById('education').style.display = 'none';
 
-document.getElementById('equity').style.top = '27%';
-document.getElementById('equity').style.left = '10%';
-document.getElementById('equity').style.width = '30%';
-document.getElementById('equity').style.animation = '1s swing ease-in-out 0s infinite alternate';
-document.getElementById('equity').style.display = 'none';
-
 document.getElementById('story').style.top = '27%';
 document.getElementById('story').style.left = '10%';
 document.getElementById('story').style.width = '30%';
 document.getElementById('story').style.animation = '1s swing ease-in-out 0s infinite alternate';
 document.getElementById('story').style.display = 'none';
 
-txt.changeWord(["space"]);
-document.getElementById('spacetxt').innerHTML = "I have always had an avid interest in outer space, the planets and extraterrestrial life! I often try to incorporate it within my work. I am really excited to see what we discover about aliens and alternate universes in the future!";
+txt.changeWord(["became a finalist"]);
+document.getElementById('spacetxt').innerHTML = "In 2020, I participated in the YouthTech startup incubator organised by StartAD, and my team was 1 of 15 teams chosen out of 98 applicants. In 2021, I was 1 of 6 people nominated to participate in StartAD's Innovention program";
+
 }
 
 //loves webpage
@@ -146,7 +139,7 @@ function flingB() {
 document.getElementById('baking').style.animation = '1s away ease-in-out 0s 1 forwards';
 document.getElementById('food').style.display = 'block';
 txt.changeWord(["food"]);
-document.getElementById('spacetxt').innerHTML = "I'm an absolute foodie! My favourite cuisines are Nigerian and East Asian. I can't even choose a favourite dish, because that's how amazing they are! I definitely recommend";
+document.getElementById('spacetxt').innerHTML = "I am an absolute foodie! My favourite cuisines are Nigerian and East Asian. I cannot even choose a favourite dish. I love learning new recipes and visiting new restaurants with my friends";
 }
 
 function flingFo() {
@@ -169,7 +162,7 @@ document.getElementById('food').style.animation = '1s swing ease-in-out 0s infin
 document.getElementById('food').style.display = 'none';
 
 txt.changeWord(["film"]);
-document.getElementById('spacetxt').innerHTML = "Watching movies and TVs shows are a part of my daily routine. Apart from the fact that they are extremely entertaining, I gain inspiration from what I watch. From cartoons to sitcoms to reality TV. My favourite TV show is definitely The Office, and Love Island is my guilty pleasure (except I feel absolutely no guilt)";
+document.getElementById('spacetxt').innerHTML = "Watching movies and TVs shows are a part of my daily routine. Apart from their entertainment value, I gain inspiration from what I watch and use that knowledge for my personal film projects. From cartoons to sitcoms to reality TV. My favourite TV show is definitely The Office, and Love Island is my guilty pleasure (except I feel absolutely no guilt)";
 }
 
 //skills webpage
@@ -181,15 +174,15 @@ document.getElementById('film2').addEventListener('onclick', flingFi2);
 function flingD() {
 document.getElementById('design').style.animation = '1s away ease-in-out 0s 1 forwards';
 document.getElementById('coding').style.display = 'block';
-txt.changeWord(["coding"]);
-document.getElementById('spacetxt').innerHTML = "A large portion of my work focuses on creative coding, such as game design, web design and digital art. I have experience coding in HTML/ CSS, Javascript, Processing and C++. I also use C# to create animations in Unity, and Arduino for electronic coding";
+txt.changeWord(["programmer"]);
+document.getElementById('spacetxt').innerHTML = "A large portion of my work focuses on creative coding, such as game development, web design (this website is coded from scratch!) and digital art. I have experience coding in HTML/ CSS, Javascript, Python, C#, Processing and C++. I also have experience using Arduino to program electronic hardware";
 }
 
 function flingC() {
 document.getElementById('coding').style.animation = '1s away ease-in-out 0s 1 forwards';
 document.getElementById('film2').style.display = 'block';
-txt.changeWord(["film"]);
-document.getElementById('spacetxt').innerHTML = "I have much experience in filmmaking, from writing scripts to editing videos. I have used Adobe Premiere Pro, DaVinci Resolve, iMovies, Filmora and Movavi to edit films";
+txt.changeWord(["filmmaker"]);
+document.getElementById('spacetxt').innerHTML = "I have much experience in filmmaking, from writing scripts to directing a shoot. I have intermediate knowledge of lighting and camera equipment, such as silks, scrims and gels. I have experience using professional film equipment such as the Blackmagic Ursa Mini, Blackmagic Pocket 4K and Canon Mirrorless cameras.  I am also skilled in video editing and colour grading using software such as Davinci Resolve and Adobe Premiere Pro";
 }
 
 function flingFi2() {
@@ -211,8 +204,8 @@ document.getElementById('film2').style.width = '30%';
 document.getElementById('film2').style.animation = '1s swing ease-in-out 0s infinite alternate';
 document.getElementById('film2').style.display = 'none';
 
-txt.changeWord(["design"]);
-document.getElementById('spacetxt').innerHTML = "I work with digital design tools such as Adobe Suite, Procreate and Canva for my projects. I also have experience working with Unity to design virtual reality spaces";
+txt.changeWord(["game developer"]);
+document.getElementById('spacetxt').innerHTML = "I am skilled in 3D modelling and have experience using Maya, ZBrush and Blender. I code in C# using Unity to develop 3D games and virtual reality experiences. I also use Adobe Suite and other platforms such as Procreate and Canva to aid in design";
 }
 
 document.getElementById('arrow1').addEventListener('onclick', nextProse);
